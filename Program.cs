8f3edebee13f789a323d10f1fc38286ca31fdd8c
Евгений Кучма <desferto87@gmail.com>
2024-06-19 22:42:24 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeWorkTenSumOfNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int number = rand.Next(0, 100);
+            int sumNumber = 0;
+
+            while (number > 0)
+            {
+                
+                if (number % 3 == 0 || number % 5 == 0)
+                {
+                    sumNumber += number;
+                }
+                number--;
+            } 
+            Console.WriteLine(sumNumber);
+            }
+        }
+    }
